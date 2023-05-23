e095000171aa9f75ad545c9f21822c59f84db390
lqmeyers <lqmeyers@gmail.com>
2023-05-23 12:15:15 -0700

    dataset tracking

--- dataset_summary_refresh.sh
+++ dataset_summary_refresh.sh
@@ -4,15 +4,7 @@
 CURRENTDATE=`date +"%Y-%m-%d-%T"`
 echo "Dataset summary last refreshed: "$CURRENTDATE > dataset_summary.txt
 echo " " >> dataset_summary.txt
-echo "current dir is " >> dataset_summary.txt
-pwd >> dataset_summary.txt
-echo " " >> dataset_summary.txt
 echo "-------------------------------------------------------------------" >> dataset_summary.txt
-echo "current files in images/" >> dataset_summary.txt
+echo "current datasets/" >> dataset_summary.txt
 echo " " >> dataset_summary.txt
-ls -R ./images/ >> dataset_summary.txt
-echo " " >> dataset_summary.txt
-echo "-------------------------------------------------------------------" >> dataset_summary.txt
-echo "current files in masks/" >> dataset_summary.txt
-echo " " >> dataset_summary.txt
-ls -R ./masks/ >> dataset_summary.txt
+ls -R ./data/ >> dataset_summary.txt
