c328dc06e2c509aae36eca8b3b82738d1ba38872
Luke Meyers <lqmeyers@gmail.com>
2026-07-14 16:57:22 -0400

    able to push to hub mixin inheritance

--- Pytorch-UNet/unet/unet_model.py
+++ Pytorch-UNet/unet/unet_model.py
@@ -1,95 +1,11 @@
 """ Full assembly of the parts to form the complete network """
 
+
+from huggingface_hub import PyTorchModelHubMixin
 from .unet_parts import *
 
-"""
-import torch
-import torch.nn as nn
 
-class UNet(nn.Module):
-    def __init__(self, in_channels=3, out_channels=1):
-        super(UNet, self).__init__()
-        
-        # Contracting Path (Encoder)
-        self.encoder1 = DoubleConv(in_channels, 64)
-        self.encoder2 = DoubleConv(64, 128)
-        self.encoder3 = DoubleConv(128, 256)
-        self.encoder4 = DoubleConv(256, 512)
-        
-        # Bottleneck
-        self.bottleneck = DoubleConv(512, 1024)
-        
-        # Expanding Path (Decoder)
-        self.decoder4 = UpConv(1024, 512)
-        self.decoder3 = UpConv(512, 256)
-        self.decoder2 = UpConv(256, 128)
-        self.decoder1 = UpConv(128, 64)
-        
-        # Output Layer
-        self.output = nn.Conv2d(64, out_channels, kernel_size=1)
-    
-    def forward(self, x):
-        # Contracting Path (Encoder)
-        x1 = self.encoder1(x)
-        x2 = self.encoder2(nn.functional.max_pool2d(x1, kernel_size=2))
-        x3 = self.encoder3(nn.functional.max_pool2d(x2, kernel_size=2))
-        x4 = self.encoder4(nn.functional.max_pool2d(x3, kernel_size=2))
-        
-        # Bottleneck
-        x5 = self.bottleneck(nn.functional.max_pool2d(x4, kernel_size=2))
-        
-        # Expanding Path (Decoder)
-        x4 = self.decoder4(torch.cat([x5, x4], dim=1))
-        x3 = self.decoder3(torch.cat([x4, x3], dim=1))
-        x2 = self.decoder2(torch.cat([x3, x2], dim=1))
-        x1 = self.decoder1(torch.cat([x2, x1], dim=1))
-        
-        # Output Layer
-        out = self.output(x1)
-        
-        return out
-    
-class DoubleConv(nn.Module):
-    def __init__(self, in_channels, out_channels):
-        super(DoubleConv, self).__init__()
-        
-        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1)
-        self.bn1 = nn.BatchNorm2d(out_channels)
-        self.relu1 = nn.ReLU(inplace=True)
-        
-        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1)
-        self.bn2 = nn.BatchNorm2d(out_channels)
-        self.relu2 = nn.ReLU(inplace=True)
-    
-    def forward(self, x):
-        out = self.conv1(x)
-        out = self.bn1(out)
-        out = self.relu1(out)
-        
-        out = self.conv2(out)
-        out = self.bn2(out)
-
-class UpConv(nn.Module):
-    def __init__(self, in_channels, out_channels):
-        super(UpConv, self).__init__()
-        self.upconv = nn.Sequential(
-            nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True),
-            nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True)
-        )
-
-    def forward(self, x):
-        return self.upconv(x)
-
-
-
-
-
-"""
-
-class UNet(nn.Module):
+class UNet(nn.Module, PyTorchModelHubMixin):
     def __init__(self, n_channels, n_classes, bilinear=False):
         super(UNet, self).__init__()
         self.n_channels = n_channels
